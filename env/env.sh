a6593bd9e950369b2410d0d144c0d79dd54fa748
lanrongqi <lancelrq@gmail.com>
2021-11-05 11:58:17 +0800

    add env

--- env/env.sh
+++ env/env.sh
@@ -2,21 +2,21 @@
 
 case "$1" in
   "start"|"up")
-    docker-compose -f docker-compose.yml -p dag_server up -d;;
+    docker-compose -f docker-compose.yml -p sikong-ren up -d;;
   "stop"|"down")
-    docker-compose -f docker-compose.yml -p dag_server down;;
+    docker-compose -f docker-compose.yml -p sikong-ren down;;
   "restart")
-    docker-compose -f docker-compose.yml -p dag_server down
-    docker-compose -f docker-compose.yml -p dag_server up -d
+    docker-compose -f docker-compose.yml -p sikong-ren down
+    docker-compose -f docker-compose.yml -p sikong-ren up -d
    ;;
   "ps")
-    docker-compose -f docker-compose.yml -p dag_server ps;; 
+    docker-compose -f docker-compose.yml -p sikong-ren ps;; 
   "logs")
-    docker-compose -f docker-compose.yml -p dag_server logs ${@:2};;
+    docker-compose -f docker-compose.yml -p sikong-ren logs ${@:2};;
   "exec")
-    docker-compose -f docker-compose.yml -p dag_server exec ${2} ${@:3};;
+    docker-compose -f docker-compose.yml -p sikong-ren exec ${2} ${@:3};;
   "bash")
-    docker-compose -f docker-compose.yml -p dag_server exec ${2} bash;;
+    docker-compose -f docker-compose.yml -p sikong-ren exec ${2} bash;;
   "default")
     echo "不支持的命令";;
 esac 
